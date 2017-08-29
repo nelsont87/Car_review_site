65308f814bf5e7df3269351dfc0c7cb6239006af
nelsont87 <nelsont87@gmail.com>
2017-08-29 14:55:11 -0700

    finished functionality

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -5,6 +5,10 @@ class CarsController < ApplicationController
   end
 
   def show
+    @car = Car.find(params[:id])
+    @user = @car.user
+    @reviews = @car.reviews
+    @review = @car.reviews.build
   end
 
   def new
@@ -12,28 +16,28 @@ class CarsController < ApplicationController
   end
 
   def edit
+    @car = Car.find(params[:id])
   end
 
   def create
-    binding.pry
     @car = Car.new(car_params)
     @car.user = current_user
     if @car.save
-      binding.pry
-      redirect_to cars, notice: 'New car was successfully created'
+      redirect_to @car, notice: 'New car was successfully created'
     else
-      binding.pry
       render :new, notice: 'Could not create new car'
     end
   end
   def update
+    @car = Car.find(params[:id])
     if @car.update(car_params)
-      redirect_to cars, notice: 'Car was successfully updated.'
+      redirect_to @car, notice: 'Car was successfully updated.'
     else
       render :edit, notice: 'Unable to update the car'
     end
   end
   def destroy
+    @car = Car.find(params[:id])
     @car.destroy
     redirect_to cars_path, notice: 'car was successfully destroyed.'
   end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,46 +1,42 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
-  def index
-    @reviews = Review.order("created_at DESC").all
-  end
-
-  def show
-  end
+  before_action :set_review, only: [:show]
 
   def new
     @review = Review.new
   end
 
-  def edit
+  def create
+    @car = Car.find(params[:review][:car_id])
+    @review = @car.reviews.build(review_params)
+
+    if @review.save
+      flash[:notice] = 'Review successfully added'
+    else
+      flash[:notice] = 'There was a problem with your review.'
+    end
+
+    render "cars/show"
   end
 
-  def create
-    @review = Review.new(review_params)
-    if @review.save
-      redirect_to @review, notice: 'New review was successfully created'
-    else
-      render :new, notice: 'Could not create new review'
-    end
-  end
   def update
     if @review.update(review_params)
       redirect_to @review, notice: 'Review was successfully updated.'
     else
-      render :edit, notice: 'Unable to update the review'
+      render :edit
     end
   end
+
   def destroy
     @review.destroy
-    redirect_to reviews_path, notice: 'review was successfully destroyed.'
+    redirect_to reviews_url, notice: 'Review was successfully destroyed.'
   end
+
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_review
       @review = Review.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def review_params
-      params.require(:review).permit(:title, :description)
+      params.require(:review).permit(:description, :car_id)
     end
 end
